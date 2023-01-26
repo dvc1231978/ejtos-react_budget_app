efab5a4a7d8d73da71f91c5256fe4a38080545d6
Desmond <dchan_2001@yahoo.com>
2023-01-25 21:01:09 -0500

    changes made from theia lab environment

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+
+
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
 
@@ -58,18 +60,19 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
-                    <input
+                    
+                    $<input 
                         required='required'
                         type='number'
                         id='cost'
+                        step= '10'
                         value={cost}
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
-                        Save
+                        Update
                     </button>
                 </div>
                 </div>
